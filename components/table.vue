9b8197cb0e4be36584af6d952e6618defd202f72
muhammaddin <muhammaddin.xoliqov1998@gmail.com>
2024-12-02 01:01:43 +0500

    project created

---
+++ components/table.vue
@@ -0,0 +1,125 @@
+<script setup lang="ts">
+const columns = [
+  {
+    key: "id",
+    label: "ID",
+  },
+  {
+    key: "name",
+    label: "Name",
+  },
+  {
+    key: "title",
+    label: "Title",
+  },
+  {
+    key: "email",
+    label: "Email",
+  },
+  {
+    key: "role",
+    label: "Role",
+  },
+];
+
+const people = [
+  {
+    id: 1,
+    name: "Lindsay Walton",
+    title: "Front-end Developer",
+    email: "lindsay.walton@example.com",
+    role: "Member",
+  },
+  {
+    id: 2,
+    name: "Courtney Henry",
+    title: "Designer",
+    email: "courtney.henry@example.com",
+    role: "Admin",
+  },
+  {
+    id: 3,
+    name: "Tom Cook",
+    title: "Director of Product",
+    email: "tom.cook@example.com",
+    role: "Member",
+  },
+  {
+    id: 4,
+    name: "Whitney Francis",
+    title: "Copywriter",
+    email: "whitney.francis@example.com",
+    role: "Admin",
+  },
+  {
+    id: 5,
+    name: "Leonard Krasner",
+    title: "Senior Designer",
+    email: "leonard.krasner@example.com",
+    role: "Owner",
+  },
+  {
+    id: 6,
+    name: "Floyd Miles",
+    title: "Principal Designer",
+    email: "floyd.miles@example.com",
+    role: "Member",
+  },
+  {
+    id: 7,
+    name: "Leonard Krasner",
+    title: "Senior Designer",
+    email: "leonard.krasner@example.com",
+    role: "Owner",
+  },
+];
+
+const q = ref("");
+
+const filteredRows = computed(() => {
+  if (!q.value) {
+    return people;
+  }
+
+  return people.filter((person) => {
+    return Object.values(person).some((value) => {
+      return String(value).toLowerCase().includes(q.value.toLowerCase());
+    });
+  });
+});
+
+const page = ref(1);
+const pageCount = 5;
+
+// const rows = computed(() => {
+//   return people.slice((page.value - 1) * pageCount, page.value * pageCount);
+// });
+
+const rows = computed(() => {
+  const start = (page.value - 1) * pageCount;
+  const end = page.value * pageCount;
+  return filteredRows.value.slice(start, end);
+});
+</script>
+
+<template>
+  <div>
+    <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
+      <UInput v-model="q" placeholder="Filter people..." />
+    </div>
+
+    <div>
+      <UTable :rows="rows" :columns="columns" />
+
+      <div
+        class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700"
+      >
+        <UPagination
+          v-model="page"
+          :page-count="pageCount"
+          :total="people.length"
+        />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-full h-screen flex justify-center items-center">
+    <Table />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
